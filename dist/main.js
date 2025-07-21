96075311c7fe152c42e3d6e3d815ceab74de0706
Estevam Gabriel Silva de Sousa <2025116TADS0023@ifpi.int>
2025-07-21 15:02:59 -0300

    Criando as funções

--- dist/main.js
+++ dist/main.js
@@ -1,18 +1,15 @@
+"use strict";
 //calculadora com menu Typescript puro
-function mostrarMenu(): string | null{
-    return prompt(
-        "Escolha uma opção:\n" +
+function mostrarMenu() {
+    return prompt("Escolha uma opção:\n" +
         "1 - somar\n" +
         "2 - Subtrair\n" +
         "3 - Multiplicar\n" +
         "4 - dividir\n" +
-        "5 - Sair\n "
-    );
+        "5 - Sair\n ");
 }
-
-function obterNumero(mensagem: string): number {
+function obterNumero(mensagem) {
     return Number(prompt(mensagem));
 }
-
 let numero1 = obterNumero("Digite o primeiro número:");
-let  numero2 = obterNumero("Dite o segundo número:");
+let numero2 = obterNumero("Dite o segundo número:");
